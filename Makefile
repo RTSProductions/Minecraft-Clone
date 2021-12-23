c44aa5c82a073be22291ffa68728ac46c5e3efa0
Regin Hurley David <reginhurleydavid@MacBook-Air.local>
2021-12-22 23:37:28 -0700

    Fixing issues with using glad

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	clang++ *.cpp -o Minecraft -I/usr/local/Cellar/glfw/3.3.4/include/ -L/usr/local/Cellar/glfw/3.3.4/lib -lglfw -framework Cocoa -framework OpenGL -framework IOKit
+	clang++ *.cpp /usr/local/Cellar/glad/src/glad.c -o Minecraft -I/usr/local/Cellar/glfw/3.3.4/include/ -L/usr/local/Cellar/glfw/3.3.4/lib -lglfw -framework Cocoa -framework OpenGL -framework IOKit -I/usr/local/Cellar/glad/include/
 
 build-app:
-	clang++ *.cpp -o Minecraft.app -I/usr/local/Cellar/glfw/3.3.4/include/ -L/usr/local/Cellar/glfw/3.3.4/lib -lglfw -framework Cocoa -framework OpenGL -framework IOKit
+	clang++ *.cpp /usr/local/Cellar/glad/src/glad.c -o Minecraft.app -I/usr/local/Cellar/glfw/3.3.4/include/ -L/usr/local/Cellar/glfw/3.3.4/lib -lglfw -framework Cocoa -framework OpenGL -framework IOKit -I/usr/local/Cellar/glad/include/
--- main.cpp
+++ main.cpp
@@ -1,8 +1,53 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 #include <iostream>
 
-int main()
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+int main(void)
 {
-    std::cout << "Hello, World!" << std::endl;
+    GLFWwindow* window;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initalize GLFW" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Totally Minecraft", NULL, NULL);
+
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initalize Glad" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(250 / 255, 119 / 255, 110 / 255, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+     glfwTerminate();
 
     return 0;
 
